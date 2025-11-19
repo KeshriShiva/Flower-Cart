5b1786ad58f752b74222a83eaa8c51e216c9d4a6
Shiva Keshri <shivakeshri07@gmail.com>
2025-11-19 13:57:10 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <Analytics/>
     <SpeedInsights>
-      <Analytics>
         <App />
-      </Analytics>
     </SpeedInsights>
   </React.StrictMode>
 );
